f399595130176807ae661a278e1542617a3ee724
cristian0135XP <munozrcristian@gmail.com>
2025-04-16 09:08:53 -0400

    Add files via upload

---
+++ validar.php
@@ -0,0 +1,22 @@
+<?php
+    include('db.php');
+
+    $USUARIO=$_POST['usuario'];
+    $PASSWORD=$_POST['password'];
+    $consulta="SELECT* FROM personal WHERE usuario='$USUARIO' and password='$PASSWORD'";
+    $resultado=mysqli_query($conexion,$consulta);
+
+    $filas=mysqli_num_rows($resultado);
+
+    if($filas){
+        header("location:index2.html");
+
+    }else{
+        include("index.html");
+        ?>
+            <h1>error de autentificacion</h1>
+        <?php
+    }
+    mysqli_free_result($resultado);
+    mysqli_close($conexion);
+?>
